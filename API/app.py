44b37e21eb17abeb978fc9b351a2ca3e79a9a256
Gattu Uday Shankar <udaygattu007@gmail.com>
2024-11-14 11:13:00 -0500

    Firstbranch (#1)
    * inital commit
    
    * 1st commit

---
+++ API/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langserve import add_routes
+import uvicorn 
+import os
+
+from langchain_community.llms import Ollama
+
+
+os.environ['OPEN_API_KEY'] = os.getenv('OPEN_API_KEY')
